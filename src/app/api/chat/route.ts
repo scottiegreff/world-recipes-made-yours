dd8b350c26eef036120f2aaf9e81ec4c26cd4556
Scott Greff <scottgreff@Scotts-MacBook-Pro.local>
2023-12-18 15:06:34 -0800

    change CORS res headers

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -43,7 +43,7 @@ export const POST = async function (req: NextRequest) {
       status: 200,
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin":"*",
+        "Access-Control-Allow-Origin":"https://world-recipes-made-yours.vercel.app",
         "Access-Control-Allow-Methods": "POST, OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
       },

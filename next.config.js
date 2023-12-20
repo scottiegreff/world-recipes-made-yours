edc542785d313ea88fd1bb18a1e4948517829698
Scott Greff <scottgreff@Scotts-MacBook-Pro.local>
2023-12-20 14:27:18 -0800

    change the Origin in next.config.js

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://www.worldrecipesmade.com",
+            value: "*",
           },
           { key: "Access-Control-Allow-Methods", value: "OPTIONS, POST" },
           {

80f6810e913c5781f126d666996d49392940f58a
Scott Greff <scottgreff@Scotts-MacBook-Pro.local>
2023-12-20 14:32:22 -0800

    change the Origin in next.config.js

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "*",
+            value: "https://world-recipes-made-yours.vercel.app/api/chat",
           },
           { key: "Access-Control-Allow-Methods", value: "OPTIONS, POST" },
           {

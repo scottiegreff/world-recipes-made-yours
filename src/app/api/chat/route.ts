9ae1d81348cef7241746a2601d7e4099908d4590
Scott Greff <scottgreff@Scotts-MacBook-Pro.local>
2024-01-02 11:53:36 -0800

    CORS

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -10,7 +10,7 @@ export const POST = async function (req: NextRequest, res: NextResponse) {
 
     const dataBody = {
       model: "gpt-4-1106-preview",
-      max_tokens: 800, //800
+      max_tokens: 80, //800
       messages: [...conversationHistory, currentUserInput],
     };
 

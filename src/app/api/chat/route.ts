f12fe0c59195cf0c78a5dc83c85184a689aa3d30
Scott Greff <scottgreff@Scotts-MacBook-Pro.local>
2024-01-02 11:30:25 -0800

    CORS

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async function (req: NextRequest, res: NextResponse) {
-
   if (req.method === "POST") {
     const body = await req.json();
     // get body from request
@@ -14,7 +13,7 @@ export const POST = async function (req: NextRequest, res: NextResponse) {
       max_tokens: 800, //800
       messages: [...conversationHistory, currentUserInput],
     };
-    
+
     // Get the origin of the request
     const origin = req.headers.get("Origin");
 
@@ -27,23 +26,26 @@ export const POST = async function (req: NextRequest, res: NextResponse) {
         "https://api.openai.com/v1/chat/completions",
         {
           method: "POST",
-          headers:
-            {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${openApiKey}`,
-            },
-            
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${openApiKey}`,
+          },
+
           body: JSON.stringify(dataBody),
+          signal: controller.signal,
         }
       );
+      clearTimeout(timeout);
 
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       const data = await response.json();
-    
+
       return new NextResponse(JSON.stringify(data));
     } catch (error) {
+      if ((error as Error).name === "AbortError")
+        console.log("Fetch request timed out");
       console.error(error);
       return new NextResponse("Internal Server Error", { status: 500 });
     }
